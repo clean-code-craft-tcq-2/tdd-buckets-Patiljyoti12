22f1d8c624ce34b95d25d1f3eacede0977dd2d92
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-03-05 14:36:25 +0530

    Update test_range_current_meausurements.py

--- test_range_current_meausurements.py
+++ test_range_current_meausurements.py
@@ -1,1 +1,16 @@
+import range_current_measurements
 
+start_range=3
+stop_range=6
+input_sample=1,2,6,7,8
+expected_result=2
+
+def test_no_of_readings_in_range(input_samples,start_range,stop_range,expected_result):
+  result=get_no_of_readings_in_range(input_samples,start_range,stop_range)
+  assert result==expected_result
+  
+if __name__ == '__main__':
+   test_no_of_readings_in_range(input_samples,start_range,stop_range,expected_result)
+
+  
+  
