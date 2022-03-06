931820cbcadd34376da95fd91acfad8051a7eb4a
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-03-06 18:45:42 +0530

    Update range_current_measurements.py

--- range_current_measurements.py
+++ range_current_measurements.py
@@ -1,7 +1,4 @@
 #Function to get no of readings in given range
 def get_no_of_readings_in_range(input_samples,start_range,stop_range):
-  count=0
-  for x in input_samples:
-    if x>=start_range and x<=stop_range:
-      count=count+1
-  return count
+  result=len(list(x for x in input_samples if start_range <= x <= stop_range))
+  return result
