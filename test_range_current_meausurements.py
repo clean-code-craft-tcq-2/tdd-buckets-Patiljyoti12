0f59a17819b74cfb425f5f72c90ee823636410d6
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-03-06 16:34:48 +0530

    Update test_range_current_meausurements.py

--- test_range_current_meausurements.py
+++ test_range_current_meausurements.py
@@ -1,17 +1,14 @@
+import unittest
 import range_current_measurements
 
 
-def test_no_of_readings_in_range(input_samples,start_range,stop_range,expected_result):
-  result=range_current_measurements.get_no_of_readings_in_range(input_samples,start_range,stop_range)
-  assert result==expected_result
-  
-if __name__ == '__main__':
-  #Testcase1
-  start_range=4
-  stop_range=5
-  input_samples=[3,3,5,4]
-  expected_result=2
-  test_no_of_readings_in_range(input_samples,start_range,stop_range,expected_result)
+class TypewiseTest(unittest.TestCase):
+    def test_no_of_readings_in_range(self):
+        self.assertTrue(range_current_measurements.get_no_of_readings_in_range([3,3,4,5],4,5) == 2)
 
-  
-  
+
+if __name__ == '__main__':
+  unittest.main()
+
+
+ 
