b51784cabffa617e4e81308af64fa4b88c5142e8
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-03-07 14:31:47 +0530

    Update test_range_current_meausurements.py

--- test_range_current_meausurements.py
+++ test_range_current_meausurements.py
@@ -6,7 +6,7 @@ class TypewiseTest(unittest.TestCase):
     def test_no_of_readings_in_range(self):
         self.assertTrue(range_current_measurements.get_no_of_readings_in_range([3,3,4,5],4,5) == 2)
         
-    def test_no_readings_in range(self):
+    def test_no_readings_in_range(self):
          self.assertTrue(range_current_measurements.get_no_of_readings_in_range([3,3,4,5,7,9],-1,-2) == 0)
 
 
