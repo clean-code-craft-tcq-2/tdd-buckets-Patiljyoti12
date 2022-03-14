cbd8765447f2b765ca9115ef3366b41bc29acd89
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-03-14 14:01:09 +0530

    Update test_range_current_meausurements.py

--- test_range_current_meausurements.py
+++ test_range_current_meausurements.py
@@ -3,11 +3,17 @@ import range_current_measurements
 
 
 class TypewiseTest(unittest.TestCase):
-    def test_no_of_readings_in_range(self):
-        self.assertTrue(range_current_measurements.get_no_of_readings_in_range([3,3,4,5],4,5) == 2)
+    def test_Rearrange_Samples(self):
+        self.assertTrue(range_current_measurements.Rearrange_Samples([3, 3, 5, 4, 10, 11, 12]) == [3, 3, 4, 5, 10, 11, 12])
         
-    def test_no_readings_in_range(self):
-         self.assertTrue(range_current_measurements.get_no_of_readings_in_range([3,3,4,5,7,9],-1,-2) == 0)
+    def test_Display_Samples_Details_onConsole(self):
+        self.assertTrue(range_current_measurements.Display_Samples_Details_onConsole([10,11,12],3) == '10-12, 3')
+    
+    def test_get_ranges_from_samples(self):
+        self.assertTrue(range_current_measurements.get_ranges_from_samples([3, 3, 5, 4, 10, 11, 12]) == [(3,5),(10,12)])
+     
+    def test_get_no_of_readings_in_range(self)
+        self.assertTrue(range_current_measurements.get_no_of_readings_in_range([10,11,12])==3)
 
 
 if __name__ == '__main__':
