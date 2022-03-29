b07b6cbf145d7bff2c1407287222ca1a10cbdf60
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-03-29 16:33:33 +0530

    Update A2D_12BitConverter.py

--- A2D_12BitConverter.py
+++ A2D_12BitConverter.py
@@ -4,3 +4,7 @@ def converttoAmps(input_samples):
     convertsample_to_Amps=[round( 10 * sample/ 4094)for sample in valid_samples]
     return convertsample_to_Amps
     
+def Check_InValidSamplesandPrintErrorMessage(input_samples):
+    invalid_samples = [sample for sample in input_samples if sample > 4094]
+    message="Samples out of range"
+    return message
