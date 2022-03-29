e7dbb9cc7dddfa9db91d3726431f7d77533cd343
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-03-29 16:23:50 +0530

    Update test_range_current_meausurements.py

--- test_range_current_meausurements.py
+++ test_range_current_meausurements.py
@@ -17,7 +17,7 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(range_current_measurements.get_no_of_readings_in_range([10,11,12])==3)
      
     def test_converttoAmps(self):
-        self.assertTrue(A2D_12BitConverter.converttoAmps([1100, 1802, 3530, 2555, 2000, 3200, 4094]) == [3, 4, 9, 6, 5, 8, 10]])
+        self.assertTrue(A2D_12BitConverter.converttoAmps([1100, 1802, 3530, 2555, 2000, 3200, 4094]) == [3, 4, 9, 6, 5, 8, 10])
     
 
 
